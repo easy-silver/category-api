6bd5932225c9f9e86ff453017dc1baa5a0379d70
Timo <developer.timo@gmail.com>
2022-02-23 19:10:22 +0900

    Feat: 카테고리 레포지토리 생성

---
+++ src/main/java/homework/jelee/categoryapi/domain/category/CategoryRepository.java
@@ -0,0 +1,7 @@
+package homework.jelee.categoryapi.domain.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
