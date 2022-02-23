6141a444a6ccb874f6247e84f843802fddd33dc1
Timo <developer.timo@gmail.com>
2022-02-23 19:10:10 +0900

    Feat: 카테고리 엔티티 생성
    - 상위/하위 카테고리를 위해 셀프 참조 구현

---
+++ src/main/java/homework/jelee/categoryapi/domain/category/Category.java
@@ -0,0 +1,34 @@
+package homework.jelee.categoryapi.domain.category;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Category {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(name = "category_name", nullable = false, length = 100)
+    private String name;
+
+    //상위 카테고리
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+
+    @Builder
+    public Category(String name, Category parent) {
+        this.name = name;
+        this.parent = parent;
+    }
+}
