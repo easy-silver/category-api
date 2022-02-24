782c6f2fc813989b7a530ca3eda38961476dbf5d
Timo <developer.timo@gmail.com>
2022-02-24 18:16:34 +0900

    Feat: 상위 카테고리로 하위 카테고리 목록 조회 메서드 추가

--- src/main/java/homework/jelee/categoryapi/domain/category/CategoryRepository.java
+++ src/main/java/homework/jelee/categoryapi/domain/category/CategoryRepository.java
@@ -2,6 +2,10 @@ package homework.jelee.categoryapi.domain.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findAllByParent(Category parent);
 
 }
