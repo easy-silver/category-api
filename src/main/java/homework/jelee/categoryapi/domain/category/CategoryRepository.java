d36072a69ee26df893b58f4b14cd4aad7992e0c0
Timo <developer.timo@gmail.com>
2022-02-24 22:24:50 +0900

    Feat: 최상위 카테고리 조회 메서드 추가

--- src/main/java/homework/jelee/categoryapi/domain/category/CategoryRepository.java
+++ src/main/java/homework/jelee/categoryapi/domain/category/CategoryRepository.java
@@ -6,6 +6,14 @@ import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    /**
+     * 최상위 카테고리 조회
+     */
+    List<Category> findAllByParentIsNull();
+
+    /**
+     * 해당 카테고리의 하위 카테고리 조회
+     */
     List<Category> findAllByParent(Category parent);
 
 }
