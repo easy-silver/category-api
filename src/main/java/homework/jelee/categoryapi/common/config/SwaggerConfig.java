ac99f057aa845e57dcf2d4c5867178431dcb873e
Timo <developer.timo@gmail.com>
2022-02-26 21:28:13 +0900

    Build: Swagger 2.0 의존성 추가 및 설정 등록

---
+++ src/main/java/homework/jelee/categoryapi/common/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package homework.jelee.categoryapi.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("CATEGORY REST API")
+                .description("[무신사 사전 과제] 카테고리 API Document")
+                .build();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("homework.jelee.categoryapi.web.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
