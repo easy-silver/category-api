0af19f8f87e602f914158d8c31019118819b5038
Timo <developer.timo@gmail.com>
2022-02-26 19:39:30 +0900

    Test: MockMvc를 활용한 통합 테스트
    - 카테고리 등록 실패 케이스 추가

--- src/test/java/homework/jelee/categoryapi/CategoryIntegrationTest.java
+++ src/test/java/homework/jelee/categoryapi/CategoryIntegrationTest.java
@@ -47,6 +47,33 @@ public class CategoryIntegrationTest {
     }
 
     @Test
+    @DisplayName("카테고리 등록 시 카테고리 이름이 비어있으면 400 상태코드를 반환한다.")
+    void crateCategoryFail() throws Exception {
+        CategoryCreateRequest request = new CategoryCreateRequest("");
+
+        mvc.perform(MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리 등록 시 상위 카테고리 ID가 유효하지 않은 경우 404 상태코드를 반환한다.")
+    void crateCategoryNotFoundParent() throws Exception {
+        CategoryCreateRequest request = new CategoryCreateRequest("반팔티셔츠", 1L);
+
+        mvc.perform(MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+
+    @Test
     @DisplayName("전체 카테고리 조회")
     void getCategories() {
         //when
